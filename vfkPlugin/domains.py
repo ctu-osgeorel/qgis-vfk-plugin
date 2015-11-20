b9477409d0fa77ce4fceb3e5de995c47f3b0ec1c
Stepan Bambula <stepan.bambula@gmail.com>
2015-11-20 18:23:15 +0100

    vfkPlugin - update

---
+++ vfkPlugin/domains.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ vfkPluginDialog
+                                 A QGIS plugin
+ Plugin umoznujici praci s daty katastru nemovitosti
+                             -------------------
+        begin                : 2015-06-11
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Stepan Bambula
+        email                : stepan.bambula@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+
+class Domains:
+    def __init__(self):
+        pass
+
+    def anoNe(self, an):
+        if an == "a":
+            return True
+        else:
+            return False
+
+    def cpCe(self, kod):
+        if kod == 1:
+            return "Číslo popisné"
+        elif kod == 2:
+            return "Číslo evidenční"
+        else:
+            return ""
+
+    def druhUcastnika(self, kod):
+        if kod == 1:
+            return "právnická osoba"
+        elif kod == 2:
+            return "fyzická osoba"
+        elif kod == 3:
+            return "ostatní"
+        elif kod == 4:
+            return "právnická osoba státní správy"
+        else:
+            return ""
+
+    def rodinnyStav(self, kod):
+        if kod == 1:
+            return "svobodný/svobodná"
+        elif kod == 2:
+            return "ženatý/vdaná"
+        elif kod == 3:
+            return "rozvedený/rozvedená"
+        elif kod == 4:
+            return "ovdovělý/ovdovělá"
+        elif kod == 5:
+            return "druh/družka"
+        else:
+            return ""
