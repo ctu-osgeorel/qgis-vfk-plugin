55a9642bc566895e9a5642646c1c335e621b685d
Martin Landa <landa.martin@gmail.com>
2016-04-16 15:36:35 +0200

    menu cosmetics (VFK)

--- resources_rc.py
+++ resources_rc.py
@@ -2,8 +2,7 @@
 
 # Resource object code
 #
-# Created: Sun Apr 10 10:08:44 2016
-#      by: The Resource Compiler for PyQt (Qt v4.8.6)
+# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
 
